c0c1ea6bdce6d85e01ae23337bcd57d5c0b721e7
shane@sidequestvr.com <shane@sidequestvr.com>
2021-01-19 23:49:01 +0000

    fixed folder creation

--- init.lua
+++ init.lua
@@ -9,10 +9,11 @@ local files = {
 local folders = {
     "htmlparser"
 }
+local fs = require("filesystem")
 local base_url = "https://raw.githubusercontent.com/SideQuestVR/OC-Browser/master";
 local mkdir = loadfile("/bin/mkdir.lua")
 for key,value in pairs(folders) do
-    mkdir("-p",value)
+    fs.makeDirectory(value)
 end
 local wget = loadfile("/bin/wget.lua")
 for key,value in pairs(files) do
