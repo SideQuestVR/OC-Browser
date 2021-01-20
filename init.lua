4be3751f83e2cf3ba2c2f146fa435d3ab2df730b
shane@sidequestvr.com <shane@sidequestvr.com>
2021-01-20 00:00:49 +0000

    added filesystem stuff

--- init.lua
+++ init.lua
@@ -7,13 +7,12 @@ local files = {
     "/htmlparser/voidelements.lua"
 }
 local folders = {
-    "htmlparser"
+    "/htmlparser"
 }
 local fs = require("filesystem")
 local base_url = "https://raw.githubusercontent.com/SideQuestVR/OC-Browser/master";
-local mkdir = loadfile("/bin/mkdir.lua")
 for key,value in pairs(folders) do
-    fs.makeDirectory(value)
+    fs.makeDirectory(shell.getWorkingDirectory()..value)
 end
 local wget = loadfile("/bin/wget.lua")
 for key,value in pairs(files) do
